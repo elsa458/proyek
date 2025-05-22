4a75f7a7c4ef93b8ee2aac0dcc6bc48b2454de2c
elsa458 <aelsa680@gmail.com>
2025-05-22 09:10:33 +0700

    Update App.py

--- App.py
+++ App.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("Hello, World!")
 st.write("Aplikasi sederhana saya dengan Streamlit dari GitHub.")
+
+st.set_page_config(page_titlle="Iris Dasboard App", layout="centered")
